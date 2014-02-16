9dd982e76ded9a30ee5d2f60c82deb566918948f
Takeshi Yabe <tyabe@nilidea.com>
2014-02-16 14:23:33 +0900

    Add spec of models

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,42 @@
 require 'spec_helper'
 
-describe "Project Model" do
-  let(:project) { Project.new }
-  it 'can be created' do
-    project.should_not be_nil
+describe Project do
+  describe 'validations' do
+    it 'valid with a name and password' do
+      expect(Project.new(name: 'test_project', password: 'secret')).to be_valid
+    end
+    it 'invalid without a name' do
+      expect(Project.new(password: 'secret')).to have(1).errors_on(:name)
+    end
+    it 'invalid without a password' do
+      expect(Project.new(name: 'test_project')).to have(1).errors_on(:password)
+    end
+    it 'is invalid if length of name is less than 0' do
+      expect(Project.new(name: '', password: 'secret')).to have(1).errors_on(:name)
+    end
+    it 'is invalid if length of name is greater than 30' do
+      expect(Project.new(name: '_'.tap{|c|6.times{c<<'test_'}}, password: 'secret')).to have(1).errors_on(:name)
+    end
+  end
+  describe 'relations' do
+    it 'has many posts' do
+      expect(Project.reflect_on_all_associations(:has_many).map(&:name)).to include(:posts)
+    end
+  end
+  describe 'callbacks' do
+    it 'generate token when model is created' do
+      project = Project.create(name: 'test_project', password: 'secret')
+      expect(project.token).to be_present
+    end
+    it 'generate crypted password when model is created' do
+      project = Project.create(name: 'test_project', password: 'secret')
+      expect(project.crypted_password).to be_present
+    end
+  end
+  describe '.has_password?' do
+    it 'can check password match' do
+      project = Project.create(name: 'test_project', password: 'secret')
+      expect(project).to be_has_password('secret')
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,3 +29,13 @@ if ENV['TRAVIS']
   require 'coveralls'
   Coveralls.wear!
 end
+
+module ::ActiveModel::Validations
+  def errors_on(attribute, options = {})
+    valid_args = [options[:context]].compact
+    self.valid?(*valid_args)
+
+    [self.errors[attribute]].flatten.compact
+  end
+  alias :error_on :errors_on
+end
