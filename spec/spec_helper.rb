3c9f30786edbab58a9705f66c43fe93934670c53
Takeshi Yabe <tyabe@nilidea.com>
2014-02-16 16:21:46 +0900

    Upgrade configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'database_cleaner'
 
-PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+RACK_ENV = 'test' unless defined?(RACK_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+ActiveRecord::Base.logger = Padrino.logger
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
